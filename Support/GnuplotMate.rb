0003fd89ef5ff2f0f8e0cfa400ac6580136ba996
Tobias Hess <me@tobiashess.de>
2011-09-02 11:49:47 +0200

    Change the GnuplotMate script to choose the right Display methode for the used terminal. Until now implemented: epslatex, lua tike

--- Support/GnuplotMate.rb
+++ Support/GnuplotMate.rb
@@ -2,7 +2,7 @@ class GnuplotMate
   
   # Properties 
   
-  attr_accessor :script, :outputFiles, :outputFilesLines
+  attr_accessor :script, :outputFiles, :outputFilesLines, :texHeader, :gpname
   
   def path
     possible_paths = [ENV["TM_GNUPLOT"], `which gnuplot`, "/opt/local/bin/gnuplot", "/sw/bin/gnuplot", "/usr/local/bin/gnuplot"]
@@ -13,6 +13,8 @@ class GnuplotMate
   
   def initialize(script)
     @script = script
+    @texHeader = ENV["TM_BUNDLE_SUPPORT"]+"/texHeader.tex"
+    @gpname = File.basename(ENV["TM_FILEPATH"],File.extname(ENV["TM_FILEPATH"]))
   end
   
   
@@ -110,132 +112,40 @@ class GnuplotMate
 
   def displayLua
     
-    #Create LaTexfile
-     gpname = File.basename(ENV["TM_FILEPATH"],File.extname(ENV["TM_FILEPATH"]))
-     latex = Tempfile.new("Plot_#{gpname}.tex")
-     latex.puts '\documentclass[fontsize=11pt]{scrartcl}'
-     latex.puts '\usepackage[utf8]{inputenc}'
-     latex.puts '\usepackage[T1]{fontenc}'
-     latex.puts '\usepackage[]{gnuplot-lua-tikz}'
-     latex.puts '\usepackage[active,tightpage]{preview}'
-     latex.puts '\PreviewEnvironment{tikzpicture}'
-     latex.puts '\setlength\PreviewBorder{2mm}'
-     latex.puts '\usepackage{lmodern}'
-     latex.puts '\usepackage[]{siunitx}'
-     latex.puts '\newcommand{\bez}[1]{\mathrm{#1}}'
-     latex.puts '\begin{document}'
-     latex.puts '\pagestyle{empty}'
-     self.outputFiles.each do |f|
-       latex.puts "\\include{#{f}} \\newpage" 
-     end
-     latex.puts '\end{document}'
-
-     #Run PDFLatex
-     latex.flush
-     puts %x{/usr/texbin/pdflatex -interaction=batchmode #{latex.path}}
-
-     #Determine Page to Display
-     currentLine =  Integer(ENV["TM_LINE_NUMBER"])
-     page = 0
-     self.outputFilesLines.each do |no|
-          if currentLine >= no
-            page = page+1
-          end
-     end
-     if page == 0
-       page = 1
-     end
-
-
-     # Open the PDFFile in Skim
-     pdfFileName = File.expand_path("Plot_#{gpname}.pdf")
-     `osascript &>/dev/null \
-        -e 'set theFile to POSIX file "#{pdfFileName}" ' \
-        -e 'tell application "Skim"' \
-        -e 'activate' \
-        -e 'open theFile' \
-        -e 'revert front document' \
-        -e 'set view settings of front document to {auto scales:true}' \
-        -e 'tell front document to go to page #{page}' \
-        -e 'end tell' &`
+    #Define Terminal Dependent Package String and Envirorment  
+    packages = '\usepackage[]{gnuplot-lua-tikz}'
+    previewEnv = 'tikzpicture'
     
-    
-    
-    
+    #Run PDFLatex
+    self.runLatexOnTempFile(packages,previewEnv)
+        
+    #Show in Skim
+    self.findCurrentPageAndShowDocumentInSkim
+   
   end
 
   def displayEpslatex
     
-     #Dertermine Size of Terminal
-     size = script.scan(/^\W*set terminal.*size (\d+\.*\d*),(\d+\.*\d*)/)
-     if size.empty?
-       puts "Error: No size in Terminal defined"
-       return
-     end
-     if size.length > 1
-       puts "Error: More than on Size defined"
-       return
-     end
-     size = size[0]
      
      # Convert eps to pdf
-     files.each do |f|
+     self.outputFiles.each do |f|
        puts %x{bash /usr/texbin/epstopdf #{f}.eps}
      end
      
-     #Create LaTexfile
-     gpname = File.basename(ENV["TM_FILEPATH"],File.extname(ENV["TM_FILEPATH"]))
-     latex = Tempfile.new("Plot_#{gpname}.tex")
-     latex.puts '\documentclass[fontsize=11pt]{scrartcl}'
-     latex.puts '\usepackage[utf8]{inputenc}'
-     latex.puts '\usepackage[T1]{fontenc}'
-     latex.puts '\usepackage[]{graphicx}'
-     latex.puts '\usepackage[]{xcolor}'
-     latex.puts "\\usepackage[papersize={#{size[0]}in,#{size[1]}in},scale=1,layoutoffset={-15pt,-2pt}]{geometry}"
-     latex.puts '\usepackage{lmodern}'
-     latex.puts '\usepackage[]{siunitx}'
-     latex.puts '\newcommand{\bez}[1]{\mathrm{#1}}'
-     latex.puts '\begin{document}'
-     latex.puts '\pagestyle{empty}'
-     files.each do |f|
-       latex.puts "\\include{#{f}} \\newpage" 
-     end
-     latex.puts '\end{document}'
+     #Define Terminal Dependent Package String and Envirorment  
+     packages = '\usepackage[]{graphicx} \usepackage[]{xcolor}' 
+     previewEnv = 'picture'
      
      #Run PDFLatex
-     latex.flush
-     puts %x{/usr/texbin/pdflatex -interaction=batchmode #{latex.path}}
+     self.runLatexOnTempFile(packages,previewEnv)
      
-     #Determine Page to Display
-     currentLine =  Integer(ENV["TM_LINE_NUMBER"])
-     page = 0
-     filesLines.each do |no|
-          if currentLine >= no
-            page = page+1
-          end
-     end
-     if page == 0
-       page = 1
-     end
-     
-     
-     # Open the PDFFile in Skim
-                              pdfFileName = File.expand_path("Plot_#{gpname}.pdf")
-                              `osascript &>/dev/null \
-                                 -e 'set theFile to POSIX file "#{pdfFileName}" ' \
-                                 -e 'tell application "Skim"' \
-                                 -e 'activate' \
-                                 -e 'open theFile' \
-                                 -e 'revert front document' \
-                                 -e 'set view settings of front document to {auto scales:true}' \
-                                 -e 'tell front document to go to page #{page}' \
-                                 -e 'end tell' &`
-                  
+     #Show in Skim
+     self.findCurrentPageAndShowDocumentInSkim
                   
     #Set Pfad in Gnuplot File Relative to Current ProjectPath
     
     if ENV["TM_PROJECT_DIRECTORY"]
-      files.each do |f|
+      self.outputFiles.each do |f|
         path = Pathname.pwd
         path = path.relative_path_from(Pathname.new(ENV["TM_PROJECT_DIRECTORY"]))
         path = File.join(path,"#{f}")
@@ -258,6 +168,57 @@ class GnuplotMate
     data.gsub!(/^set output.*$/) { "set term aqua #{plotnum += 1}" }
     puts data
     execute data
+  end
+  
+  def runLatexOnTempFile(packages,previewEnv)
+     
+     latex = Tempfile.new("Plot_#{self.gpname}.tex")
+     latex.puts '\documentclass[fontsize=11pt]{scrartcl}'
+     latex.puts packages
+     latex.puts '\usepackage[active,tightpage]{preview}'
+     latex.puts "\\PreviewEnvironment{#{previewEnv}}"
+     latex.puts '\setlength\PreviewBorder{2mm}'
+     latex.puts File.read(self.texHeader)
+     latex.puts '\begin{document}'
+     latex.puts '\pagestyle{empty}'
+     self.outputFiles.each do |f|
+       latex.puts "\\include{#{f}} \\newpage" 
+     end
+     latex.puts '\end{document}'
+     
+     #Run PDFLatex
+     latex.flush
+     puts %x{/usr/texbin/pdflatex -interaction=batchmode #{latex.path}}
+     latex.close
+  end
+  
+  def findCurrentPageAndShowDocumentInSkim
+    
+    #Determine Page to Display
+     currentLine =  Integer(ENV["TM_LINE_NUMBER"])
+     page = 0
+     self.outputFilesLines.each do |no|
+          if currentLine >= no
+            page = page+1
+          end
+     end
+     if page == 0
+       page = 1
+     end
+         
+     # Open the PDFFile in Skim
+     pdfFileName = File.expand_path("Plot_#{self.gpname}.pdf")
+     puts pdfFileName
+     `osascript &>/dev/null \
+     -e 'set theFile to POSIX file "#{pdfFileName}" ' \
+     -e 'tell application "Skim"' \
+     -e 'activate' \
+     -e 'set theDocument to open theFile' \
+     -e 'revert theDocument' \
+     -e 'set view settings of theDocument to {auto scales:true}' \
+     -e 'tell theDocument to go to page #{page}' \
+     -e 'end tell' &`
+
   end
 
 
